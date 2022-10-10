44a5849a1e59516a45d8aee655df1282cdf730a1
Bird97 <juamacx@gmail.com>
2022-10-10 05:21:35 -0500

    Redux ok :D

---
+++ client/src/redux/actions.js
@@ -0,0 +1,128 @@
+import axios from "axios"
+
+// -------------------> ACTIONS <-------------------
+
+export const GET_RECIPES = "GET_RECIPES"; // Ya :D
+export const GET_DIETS ="GET_DIETS"; //Ya :D
+export const GET_RECIPES_BY_NAME = "GET_RECIPES_BY_NAME"; // Ya :D
+export const POST_RECIPE ="POST_RECIPE";
+export const GET_DETAILS ="GET_DETAILS"; //Ya :D
+export const ORDER_RECIPES_BY_NAME ="ORDER_RECIPES_BY_NAME"; //Ya :D
+export const ORDER_RECIPES_BY_SCORE ="ORDER_RECIPES_BY_SCORE"; //Ya :D
+export const FILTER_BY_DIET ="FILTER_BY_DIET"; //Ya :D
+export const RESET = "RESET"; //Ya :D
+export const RESET_DETAIL = "RESET_DETAIL";
+
+
+//-------------------> RUTES <-------------------
+export const URL_RECIPES = "http://localhost:3001/recipes"
+export const URL_POST_RECIPE = "http://localhost:3001/recipes/create"
+export const URL_DIETS = "http://localhost:3001/diets"
+
+export function getAllRecipes() {
+  return async function (dispatch) {
+    try {
+      let jsonRecipes = await axios.get(URL_RECIPES);
+      return dispatch({
+        type: GET_RECIPES,
+        payload: jsonRecipes.data,       
+      });
+    } catch (error) {
+      console.log(error.message);
+      return alert(
+        "Ay wey... :( Hubo un error al cargar la informacion. Intenta en unos minutos"
+      );
+    }
+  };
+}
+
+export function getDiets(){
+  return async function (dispatch){
+    try{
+      let diets= await axios.get(URL_DIETS);
+      return dispatch({
+        type: GET_DIETS,
+        payload: diets.data,       
+      });      
+    }catch(e){
+      console.log(e.message);
+      return alert(
+        ":( Hubo un error al cargar la informacion. Intenta en unos minutos"
+      );
+    }
+  }
+}
+
+export function getNameRecipe(name) {
+  return async function (dispatch) {
+    try {
+      if (name.search(/^[a-zA-Zñáéíóúü]*$/)) {
+        return alert("El nombre a buscar solo debe contener letras.");
+      }
+      let jsonRecipes = await axios.get(URL_RECIPES+"?name="+name);
+      
+      if (jsonRecipes.data === "No se encontró receta") { 
+        alert(`No se encontró receta con el nombre: ${name}`);
+      }
+      return dispatch({
+        type: GET_RECIPES_BY_NAME,
+        payload: jsonRecipes.data,
+      });
+    } catch (e) {
+      return alert(`No se encontró Receta con el nombre: ${name}`);
+    }
+  };
+}
+
+export function getDetails(id){
+  return async function (dispatch) {
+    try {
+      let jsonRecipeID = await axios.get(
+        `http://localhost:3001/recipes/${id}`
+      );
+
+      return dispatch({
+        type: GET_DETAILS,
+        payload: jsonRecipeID.data,
+      });
+    } catch (error) {
+      return alert(`No encontramos Receta el ID ${id}.`);
+    }
+  };
+}
+
+// export function resetDetail() {
+//   return {
+//     type: RESET_DETAIL,
+//   };
+// }
+
+export function reset() {
+  return {
+    type: RESET,
+  };
+}
+
+// // -------------------> ORDENAMIENTOS <-------------------
+export function orderByScore(payload) {
+  return {
+    type: ORDER_RECIPES_BY_SCORE,
+    payload,
+  };
+}
+
+export function orderByName(payload) {
+  return {
+    type: ORDER_RECIPES_BY_NAME,
+    payload,
+  };
+}
+
+// // -------------------> FILTRO <-------------------
+
+export function  filterByDiet(payload){
+  return {
+    type: FILTER_BY_DIET,
+    payload,
+  };
+}
---
+++ client/src/redux/reducers.js
@@ -0,0 +1,120 @@
+import {
+    GET_RECIPES, //Ya :D
+    GET_DIETS, //Ya :D
+    POST_RECIPE, 
+    GET_DETAILS, //Ya :D
+    ORDER_RECIPES_BY_NAME, //Ya :D
+    ORDER_RECIPES_BY_SCORE, //Ya :D
+    FILTER_BY_DIET, //creo que Ya :D
+    RESET, //Ya :D
+    RESET_DETAIL,
+    GET_RECIPES_BY_NAME //Ya :D
+
+} from "./actions"
+
+const initialState = {
+  recipes:[],
+  recipesAll:[],
+  diets: [],
+  detail: [],
+}
+
+export function rootReducer(state=initialState,action){
+  
+  switch(action.type){
+    case GET_RECIPES:
+      return {
+        ...state,
+        recipes:action.payload,
+        recipesAll: action.payload
+      };  
+    case GET_DIETS:
+      return {
+        ...state,
+        diets:action.payload,
+      };
+    case POST_RECIPE:
+      return state;
+
+    case GET_DETAILS:
+      return { ...state, 
+        detail: action.payload 
+      };
+
+    case ORDER_RECIPES_BY_NAME:
+      
+    let recipesOrderByName =
+          action.payload !== "ZtoA"
+            ? state.recipes.sort(function (a, b) {
+                if (a.name > b.name) return 1;
+                if (b.name > a.name) return -1;
+                return 0;
+              })
+            : state.recipes.sort(function (a, b) {
+                if (a.name > b.name) return -1;
+                if (b.name > a.name) return 1;
+                return 0;
+              });
+        return {
+          ...state,
+          recipes: recipesOrderByName,
+        };
+        
+    case ORDER_RECIPES_BY_SCORE:
+      let recipesOrderByScore =
+          action.payload === "minToMax"
+            ? state.recipes.sort(function (a, b) {
+                if (a.score > b.score) return 1;
+                if (b.score > a.score) return -1;
+                return 0;
+              })
+            : state.recipes.sort(function (a, b) {
+                if (a.score > b.score) return -1;
+                if (b.score > a.score) return 1;
+                return 0;
+              });
+        return {
+          ...state,
+          recipes: recipesOrderByScore,
+        };
+
+    case FILTER_BY_DIET:
+      {
+        let recipesByDiet = recipesAll.filter(
+          recipe=> recipe.diets.includes(action.payload)
+        );
+        return {
+          ...state,
+          recipes:recipesByDiet
+        }
+      }
+       
+
+    case GET_RECIPES_BY_NAME:
+      let searched=action.payload;
+      if(searched!=="No se encontró receta"){
+        return{
+          ...state,
+          recipes:searched,
+        }
+      }else{
+        return {
+          ...state,
+          recipes:false,
+        }
+      }
+
+    case RESET:
+      return {
+        ...state,
+        recipes:recipesAll,
+      }
+  
+    case RESET_DETAIL:
+      return state;
+    
+      default:
+      return state;
+
+  }
+}
